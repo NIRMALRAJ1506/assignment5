b1188fe5f2e2bfd5fb210a2ccda7080e1361ba01
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2023-11-09 16:32:33 +0530

    First Commit of assignment 5

---
+++ Assignment 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height in cm");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Name: " + name + "\nAge: " + age + "\nHeight: " + height);
+            Console.ReadKey();
+        }
+    }
+}
